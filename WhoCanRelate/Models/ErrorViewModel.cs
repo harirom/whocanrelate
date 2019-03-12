702c110ee6a794c2cbf17ac415b8772d92e0c236
harirom <>
2019-03-12 19:52:43 +0530

    InitialVersion
    InitialVersion

---
+++ WhoCanRelate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WhoCanRelate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
