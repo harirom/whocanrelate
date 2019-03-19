286d486f6c1b330e8cbf3607ef3fcdd0052185a7
harirom <>
2019-03-19 19:25:16 +0530

    Version0.3
    Version0.3

---
+++ WhoCanRelate/Models/ContentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhoCanRelate.Models
+{
+    public class ContentModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public byte[] Image { get; set; }
+        public string Body  { get; set; }
+        public string Tags  { get; set; }
+        public string Author { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
